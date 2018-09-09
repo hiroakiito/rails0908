7768751388452e269e29f17b17e6dbb268209ee4
EC2 Default User <ec2-user@ip-172-31-36-115.ap-southeast-1.compute.internal>
2018-09-09 15:37:23 +0000

    メール設定

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,14 +3,12 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attribute(:activated,    true)
-      user.update_attribute(:activated_at, Time.zone.now)
       user.activate
       log_in user
-      flash[:success] = "アカウント認証に成功しました"
+      flash[:success] = "アカウントが有効化されました"
       redirect_to user
     else
-      flash[:danger] = "リンクが有効ではありません"
+      flash[:danger] = "リンクが無効になっています"
       redirect_to root_url
     end
   end
